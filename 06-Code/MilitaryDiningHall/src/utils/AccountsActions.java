42ae21fd53a8d7317c6e256ee73ea14e79834db0
carlitos2422k <carlospe0505@gmail.com>
2024-06-05 19:49:31 -0500

    Update code

--- 06-Code/MilitaryDiningHall/src/utils/AccountsActions.java
+++ 06-Code/MilitaryDiningHall/src/utils/AccountsActions.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package utils;
 
 import ec.edu.espe.militarydininghall.model.Accounts.Account;
